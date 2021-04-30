be5346afc67cb93b35cbf73a1972cfb3fc72288d
ChaseC-130 <chase.c.cargill@gmail.com>
2021-04-30 10:33:32 -0500

    Ad-hoc off

--- CardenApi.py
+++ CardenApi.py
@@ -34,4 +34,5 @@ def receive():
     return process_text(request)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, ssl_context='adhoc')
+    app.run(host='0.0.0.0', port=8000)
+    #, ssl_context='adhoc')
