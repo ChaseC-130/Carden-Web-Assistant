5ba61a66d17a6abf4f3ea0846ed6293e22c78226
ChaseC-130 <chase.c.cargill@gmail.com>
2021-04-30 12:31:58 -0500

    Weather command improvement

--- Commands.py
+++ Commands.py
@@ -14,9 +14,9 @@ def process_text(request):
         response['response'] = 'I will now play{} on YouTube.'.format(song)
         response['action'] = get_song(song)
 
-    if 'weather' in text:
+    if 'weather in' in text:
         try:
-            response['response'] = get_weather(text)
+            response['response'] = get_weather(text.split('weather in', 1)[1])
         except KeyError:
             response['response'] = 'I was unable to find the weather.'
         
